45700843bf77df61d711d7886778c0a0255c9bdc
Chris Wadsworth <cjwadswo@uno.edu>
2022-12-07 15:07:49 -0600

    Create todo module

---
+++ src/todos.js
@@ -0,0 +1,15 @@
+import { compareAsc, format } from "date-fns";
+
+const todos = function (
+  title,
+  desc = "no description",
+  dueDate = "none",
+  priority = "none",
+  completed = "false"
+) {
+  if (title === undefined) {
+    throw new Error("No title");
+  }
+
+  return { title, desc, dueDate, priority, completed };
+};
