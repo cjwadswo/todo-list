313bfad4674129d60e8bdfea29c1a893d2478eee
Chris Wadsworth <cjwadswo@uno.edu>
2023-06-06 20:07:15 -0500

    Add functionality to the edit button
    When you click the edit button for a todo it will now populate the fields
    with the old todo's data.

--- src/controller.js
+++ src/controller.js
@@ -2,11 +2,14 @@ import todo from "./todos.js";
 import project from "./projects.js";
 import storage from "./storage.js";
 import view from "./view.js";
+import { parse, format } from "date-fns";
 //a array that sorts even number
 const controller = function (data) {
   let storage = data;
   let viewInstance = view();
   let currentProject = storage.getProjects()[0];
+  const todoListContainer = document.getElementById("todos-container");
+  const editElement = document.getElementById("edit-todo");
 
   function createTodo(formData) {
     let title = formData.get("todo-title");
@@ -23,8 +26,8 @@ const controller = function (data) {
   }
 
   function addTodo(todo) {
-    if (!storage.getProjects()[0].todoExists(todo)) {
-      storage.getProjects()[0].addTodo(todo);
+    if (!currentProject.todoExists(todo)) {
+      currentProject.addTodo(todo);
       viewInstance.addTodo(todo);
       //Add event listeners to newly added todo
       addEventListeners();
@@ -42,12 +45,14 @@ const controller = function (data) {
   function setup() {
     viewInstance.initialLoad(storage.getProjects()[0]);
     const form = document.getElementById("add-form");
+    const editSubmitBtn = document.getElementById("edit-submit");
     form.addEventListener("submit", (event) => {
       event.preventDefault();
       const formData = new FormData(form);
       createTodo(formData);
       form.reset();
     });
+    editSubmitBtn.addEventListener("submit", (event) => {});
     addEventListeners();
   }
 
@@ -68,7 +73,23 @@ const controller = function (data) {
     //TODO add edit
     const edit = document.getElementsByClassName("edit");
     [...edit].forEach((button) => {
-      let editIndex = [...edit].indexOf(button);
+      button.addEventListener("click", (event) => {
+        //Toggle the edit todo element to be visible.
+        editElement.classList.toggle("hide");
+        todoListContainer.classList.toggle("blur");
+        editElement.classList.toggle("keepfocus");
+        //Add an event listener to the edit-submit button
+        let editIndex = [...edit].indexOf(button);
+        let todoToEdit = edit[editIndex].parentNode;
+        let oldTitle = todoToEdit.querySelector(".title").textContent;
+        let oldDesc = todoToEdit.querySelector(".description").textContent;
+        let oldDate = todoToEdit.querySelector(".due-date").textContent;
+        oldDate = parse(oldDate, "MM/dd/yyyy", new Date());
+        oldDate = format(oldDate, "yyyy-MM-dd");
+        editElement.querySelector("#todo-due-date").value = oldDate;
+        editElement.querySelector("#edit-title").value = oldTitle;
+        editElement.querySelector("#edit-description").value = oldDesc;
+      });
     });
   }
 
